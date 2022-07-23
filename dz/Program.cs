d8c919dad8d1b4de82f2e02e462653389c02d12e
Nata <Beloborodova.n.s@bk.ru>
2022-07-24 00:25:34 +0300

    dz

---
+++ dz/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 27: Напишите программу, которая принимает 
+//на вход число и выдаёт сумму цифр в числе.
+
+
+int A;
+Console.WriteLine("Введите число A: ");
+A = Convert.ToInt32(Console.ReadLine());
+int sum = A%10;
+while (A > 0)
+{ 
+   A = A/10;
+   sum = sum + A%10;
+}
+{
+   Console.WriteLine (sum);
+}
+
